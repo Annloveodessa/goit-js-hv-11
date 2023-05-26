de84086d4d09ac840390caffa0b5caece9901ae6
Annloveodessa <anjel1112@gmail.com>
2023-05-26 15:15:21 +0200

    update

--- src1/refs.js
+++
@@ -1,5 +0,0 @@
-export default {
-  inputEl: document.getElementById('search-box'),
-  countryListEl: document.querySelector('.country-list'),
-  countryInfoEl: document.querySelector('.country-info'),
-};
